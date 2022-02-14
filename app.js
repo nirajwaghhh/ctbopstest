f6186848656a05c691601bb48a9f426b2a9439d4
Niraj Wagh <niraj.wagh@coupa.com>
2022-02-14 12:12:02 +0530

    complex

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
- 
-  res.send("HELLO !!!!!!!!");
+    x=0;
+    while(x<500){
+	var y= 2001020302103 * 1231312312323 * 123213 / 12 * 212312312321323 % 12 /12 /15/12 * 2342342343243;
+	x++;
+	}  
+	res.send("HELLO !!!!!!!!" + y);
 });
 
 app.listen(PORT, () => {
