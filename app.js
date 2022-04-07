76bc97f8a4a48c8706a0ecd6292471e9dc9182a5
Niraj Wagh <niraj.wagh@coupa.com>
2022-04-07 15:19:10 +0530

    added IP module

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const request = require('request');
 
 app.get("", (req, res) => {
     
-	res.send("HELLO CTB-OPSTEST !!!!!!!!" );
+	
 
 	request('https://checkip.amazonaws.com', function (error, response, body) {
 	console.error('error:', error); // Print the error if one occurred
 	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 	console.log('body:', body); // Print the HTML for the Google homepage.
+	res.send(body);
 });
 });
 
