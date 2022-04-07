8f1061dabff1a5953a4547b55b2b009520515f49
Niraj Wagh <niraj.wagh@coupa.com>
2022-04-07 15:22:59 +0530

    added ip module

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const app = require("express")();
-
 const PORT = process.env.PORT || 3000;
-
 const request = require('request');
 
 app.get("", (req, res) => {
     
-	
-
 	request('https://checkip.amazonaws.com', function (error, response, body) {
-	console.error('error:', error); // Print the error if one occurred
-	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	console.log('body:', body); // Print the HTML for the Google homepage.
 	res.send(body);
 });
 });
