d06fae32ab79583fda0d5669b5f8a90536664783
Niraj Wagh <niraj.wagh@coupa.com>
2022-04-07 15:17:16 +0530

    added IP module

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ const app = require("express")();
 
 const PORT = process.env.PORT || 3000;
 
+const request = require('request');
+
 app.get("", (req, res) => {
-    x=0;
-    while(x<5000){
-	var y= (x *1000) + 123213213321213 * 2001020302103 * 1231312312323 * 123213 / 12 * 212312312321323 % 12 /12 /15/12 * 2342342343243;
-	x++;
-	z=0;
-	while ( z < 50000)
-	{
-		var y= (x *1000) + 123213213321213 * 2001020302103 * 1231312312323 * 123213 / 12 * 212312312321323 % 12 /12 /15/12 * 2342342343243;
-        	z++;
-	}  
-    }
-	res.send("HELLO CTB-OPSTEST !!!!!!!!" + y);
+    
+	res.send("HELLO CTB-OPSTEST !!!!!!!!" );
+
+	request('https://checkip.amazonaws.com', function (error, response, body) {
+	console.error('error:', error); // Print the error if one occurred
+	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	console.log('body:', body); // Print the HTML for the Google homepage.
+});
 });
 
 app.listen(PORT, () => {
